64a51c73087a213c83f9f10602d31242a4f04eba
Rohit Ghosh <rht.ghs10@gmail.com>
2025-10-30 21:40:32 +0530

    loader

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
 import authReducer from './authSlice'
+import loadingReducer from './loadingSlice'
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
+    loading: loadingReducer,
   },
 })
 
